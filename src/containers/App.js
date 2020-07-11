7a1a3515699bbbb1c07f08e35414983157ad7fd9
pianista001 <64804893+pianista001@users.noreply.github.com>
2020-07-11 23:20:35 +0200

    Add files via upload

---
+++ src/containers/App.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry'
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchfield: ''
+    }
+  }
+
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response=> response.json())
+  .then(users => this.setState({robots: users}));
+}
+  onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value})
+      }
+render () {
+  const filteredRobot = this.state.robots.filter(robot =>{
+  return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+})
+if (!this.state.robots.length) {
+  return <h1>LOADING</h1>
+} else {
+  return (
+  <div className='tc'>
+  <h1 className="f1">Roboamigos</h1>
+  <SearchBox searchChange={this.onSearchChange}/>
+  <Scroll>
+    <ErrorBoundry>
+    <CardList robots={filteredRobot}/>
+    </ErrorBoundry>
+  </Scroll>
+  </div>
+  );
+  }
+}
+}
+
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,45 @@
+export const robots = [
+  {
+    id:1,
+    name: 'John Lennon',
+    username: 'Jonny',
+    email: 'jonnyloco@gmail.com'
+  },
+  {
+    id:2,
+    name: 'Brava Molina',
+    username: 'Bravis',
+    email: 'bravimo@gmail.com'
+  },
+  {
+    id:3,
+    name: 'Kolano Melano',
+    username: 'Meloano',
+    email: 'komelano@gmail.com'
+  },
+  {
+    id:4,
+    name: 'Rosa Melcacho',
+    username: 'melorosa',
+    email: 'elcachomelo@gmail.com'
+  },
+  {
+    id:5,
+    name: 'Mama Laganda',
+    username: 'Malaga',
+    email: 'malagana@gmail.com'
+  },
+  {
+    id:6,
+    name: 'Nelor Davolio',
+    username: 'Devilo',
+    email: 'nedavolio@gmail.com'
+  },
+  {
+    id:7,
+    name: 'Nonoma Melonona',
+    username: 'jjadns',
+    email: 'nolemona@gmail.com'
+  },
+
+]
